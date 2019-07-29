9bad5bb60213b604ec8b29f6b4dd9329659e91ee
NikitaNeganov <nikitaneganov@icloud.com>
2019-07-29 18:41:05 +0500

    Create styles.js

---
+++ styles.js
@@ -0,0 +1,20 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+
+  animationContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'row',
+    marginBottom: 8,
+  },
+
+  indicator: {
+    height: 7,
+    width: 7,
+    borderRadius: 2,
+  },
+});
